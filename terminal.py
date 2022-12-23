5847b4076b58d4c9cb19f8bc62c6f92f12a6a114
Redempt1onzzZZ <84373897+Redmept1on@users.noreply.github.com>
2022-12-23 12:03:42 +0800

    Add files via upload

---
+++ terminal.py
@@ -0,0 +1,19 @@
+from model import Model
+
+vocab_file='couplet/vocabs'
+model_dir='couplet_output'
+
+m = Model(
+        None, None, None, None, vocab_file,
+        num_units=1024, layers=4, dropout=0.2,
+        batch_size=32, learning_rate=0.0001,
+        output_dir=model_dir,
+        restore_model=True, init_train=False, init_infer=True)
+
+def chat_couplet(in_str):
+    if len(in_str) == 0 or len(in_str) > 50:
+        output = u'您的输入太长了'
+    else:
+        output = m.infer(' '.join(in_str))
+        output = ''.join(output.split(' '))
+    print('上联：%s；下联：%s' % (in_str, output))
---
+++ test.py
@@ -0,0 +1,4 @@
+import terminal
+
+in_str=input('请输入：')
+output=terminal.chat_couplet(in_str)
